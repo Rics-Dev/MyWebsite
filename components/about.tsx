800c70503534f59c2ed993241237bed358d6c28f
Racim-Fethallah <RacimFethallah@proton.me>
2023-11-04 18:50:14 +0100

    up

--- components/about.tsx
+++ components/about.tsx
@@ -24,7 +24,7 @@ export default function About() {
 
             <h2 className='text-3xl font-medium capitalize mb-8'>À propos</h2>
             <p className='mb-3 border-[0.1rem] border-blue-950  rounded-full px-20 py-10'>
-                Actuellement étudiant en première année de master en informatique à l&apos;Université Abderrahmane Mira de Béjaïa, avec une spécialité en Systèmes d&apos;Information Avancés, mon parcours académique est motivé par une passion pour la technologie et le développement de logiciels sur diverses plateformes, notamment mobiles, desktop et web. Je cherche à perfectionner mes compétences afin de devenir un professionnel accompli dans le domaine de l&apos;informatique.
+                Actuellement étudiant en première année de master en informatique à l&apos;Université des sciences et de la technologie Houari Boumédiène, en spécialité en Ingénierie du logiciel, mon parcours académique est motivé par une passion pour la technologie et le développement de logiciels sur diverses plateformes, notamment mobiles, desktop et web. Je cherche à perfectionner mes compétences afin de devenir un professionnel accompli dans le domaine de l&apos;informatique.
             </p>
         </motion.section>
     )
