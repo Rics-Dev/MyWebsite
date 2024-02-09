7749fa5832d10ca2778f8889e1199c16c413f73e
RacimFethallah <RacimFethallah@proton.me>
2024-02-09 16:34:28 +0100

    Mise à jour des variables d'environnement pour utiliser NEXT_PUBLIC_

--- lib/environement.ts
+++ lib/environement.ts
@@ -1,5 +1,5 @@
-export const sanityProjectId = process.env.SANITY_STUDIO_PROJECT_ID;
-export const sanityDataset = process.env.SANITY_STUDIO_DATASET;
-export const sanityApiVersion = process.env.SANITY_STUDIO_API_VERSION;
+export const sanityProjectId = process.env.NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID;
+export const sanityDataset = process.env.NEXT_PUBLIC_SANITY_STUDIO_DATASET;
+export const sanityApiVersion = process.env.NEXT_PUBLIC_SANITY_STUDIO_API_VERSION;
 export const sanityToken = process.env.SANITY_STUDIO_TOKEN;
 export const resendApiKey = process.env.RESEND_API_KEY;
