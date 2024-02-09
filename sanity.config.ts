be9e98318ebc49ee8ce62b4711a562ee4775daf8
RacimFethallah <RacimFethallah@proton.me>
2024-02-09 16:09:35 +0100

    Mise à jour des configurations de Sanity pour utiliser les variables d'environnement publiques

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,13 +4,13 @@ import { structureTool } from 'sanity/structure';  // Import structureTool
 import { schemaTypes } from './backend_sanity/schemas';
 
 const config = defineConfig({
-  projectId: process.env.NEXT_SANITY_PROJECT_ID ?? "",
-  dataset: process.env.NEXT_SANITY_DATASET ?? "",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? "",
   title: "RicPortfolio",
-  apiVersion: process.env.NEXT_SANITY_API_VERSION ?? "2024-02-08",
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION ?? "2024-02-08",
   basePath: "/admin",
   useCdn: false,
-  token: process.env.NEXT_SANITY_TOKEN ?? "",
+ token: process.env.NEXT_PUBLIC_SANITY_TOKEN ?? "",
   schema: {
     types: schemaTypes,
   },
